d96b091eee68c989e6d3f9a73c65325c31ff2e2f
Isabella Basso <isabbasso@riseup.net>
2022-11-12 17:14:34 -0300

    lda: add logging module and basic options parsing
    Signed-off-by: Isabella Basso <isabbasso@riseup.net>

--- lda.py
+++ lda.py
@@ -1,4 +1,10 @@
+import logging
 import pandas as pd
+import os
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.WARNING)
+LOGGER = logging.getLogger()
+
 
 class Cloud:
     """
@@ -22,6 +28,8 @@ class Cloud:
 
         long_string = ','.join(list(words))
 
+        logging.info('Generating word cloud')
+
         wordcloud = WordCloud(background_color="white", max_words=5000,
                               contour_width=3, width=1600, height=800,
                               contour_color='steelblue').generate(long_string)
@@ -39,7 +47,8 @@ class LDA:
     """
     Generate a LDA model from the given data
     """
-    def __init__(self, alpha=0.5, eta=0.5, topics=10):
+    def __init__(self, input_file, alpha=0.5, eta=0.5, topics=10):
+        self.input_file = input_file
         self.alpha = alpha
         self.eta = eta
         self.topics = topics
@@ -88,21 +97,20 @@ class LDA:
 
         data = lemmatization(data)
 
-        print("Data entries after LDA prepare")
-        print(data[:1][0][:30])
+        logging.debug("Data entries after LDA prepare")
+        logging.debug(data[:1][0][:30])
 
         id2word = corpora.Dictionary(data)
         corpus = [id2word.doc2bow(text) for text in data]
 
-        print("Corpus entries after LDA prepare")
-        print(data[:1][0][:30])
+        logging.debug("Corpus entries after LDA prepare")
+        logging.debug(data[:1][0][:30])
 
-        return corpus, id2word
+        return data, corpus, id2word
 
     def train(self, corpus, id2word,
               alpha=None, eta=None, topics=None):
         from gensim.models import LdaMulticore
-        from pprint import pprint
 
         if alpha is None:
             alpha = self.alpha
@@ -112,11 +120,13 @@ class LDA:
             topics = self.topics
 
         lda_model = LdaMulticore(corpus=corpus, id2word=id2word,
-                             num_topics=topics, random_state=100,
-                             chunksize=100, passes=10, alpha=alpha, eta=eta,
-                             per_word_topics=True, workers=15)
+                                 num_topics=topics, random_state=100,
+                                 chunksize=100, passes=10, alpha=alpha,
+                                 eta=eta, per_word_topics=True, workers=15)
 
-        pprint(lda_model.print_topics())
+        if LOGGER.getEffectiveLevel() == logging.DEBUG:
+            from pprint import pprint
+            pprint(lda_model.print_topics())
 
         return lda_model
 
@@ -124,6 +134,7 @@ class LDA:
         from gensim.utils import ClippedCorpus
         import numpy as np
         import tqdm
+        from datetime import datetime
 
         corpus_sets = [ClippedCorpus(corpus, int(len(corpus) * 0.75)), corpus]
         corpus_title = ['75% Corpus', '100% Corpus']
@@ -161,6 +172,8 @@ class LDA:
             for k in topics_range:
                 for a in alpha:
                     for b in eta:
+                        logging.info("Running LDA with k=%d, a=%s, b=%s" % (k, a, b))
+
                         lda_model = self.train(corpus_sets[i], id2word,
                                                a, b, k)
                         cv = model_perplexity(data, id2word, lda_model)
@@ -173,14 +186,17 @@ class LDA:
 
                         pbar.update(1)
 
-        pd.DataFrame(model_results).to_csv('./scores/lda_tuning_results.csv', index=False)
+        current_time = datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
+        input_file = self.input_file.split("/")[-1].split(".")[0]
+        tuning_results_file = os.path.join('./scores/lda_tuning_results-' + input_file + current_time + '.csv')
+        pd.DataFrame(model_results).to_csv(tuning_results_file, index=False)
         pbar.close()
+        logging.info("LDA tuning results saved to %s" % tuning_results_file)
 
     def visualize(self, corpus, id2word, lda_model):
         import pyLDAvis
         import pyLDAvis.gensim_models as gensimvis
         import pickle
-        import os
 
         # pyLDAvis.enable_notebook()
         LDAvis_filepath = os.path.join('./ldavis_prepared_' + str(self.topics))
@@ -198,23 +214,50 @@ class LDA:
 
         # pyLDAvis.show(LDAvis_prepared)
 
-def main():
-    readme_data = pd.read_csv('repos/repo_readmes.csv')
-    # print(readme_data.head())
+def main(args):
+    readme_data = pd.read_csv(args.input_file)
+    logging.debug(readme_data.head())
     readme_data = readme_data.drop(columns=['filename'], axis=1)
-    # print(readme_data.head())
+    logging.debug(readme_data.head())
 
-    # cloud = Cloud()
-    # words = cloud.prepare(readme_data)
-    # cloud.gencloud(words)
+    if args.cloud:
+        cloud = Cloud()
+        words = cloud.prepare(readme_data)
+        cloud.gencloud(words)
 
-    lda = LDA(0.8, 0.8, 10)
+    lda = LDA(args.input_file, 0.8, 0.8, 10)
     data, corpus, id2word = lda.prepare(readme_data)
 
-    # lda.test_model_hyperparams(data, corpus, id2word)
-    lda_model = lda.train(corpus, id2word)
-    lda.visualize(corpus, id2word, lda_model)
+    if args.hyperparams:
+        lda.test_model_hyperparams(data, corpus, id2word)
+    else:
+        lda_model = lda.train(corpus, id2word)
+        lda.visualize(corpus, id2word, lda_model)
 
 
 if __name__ == '__main__':
-    main()
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+    parser = ArgumentParser(prog='lda.py',
+                            description='Latent Dirichlet Analysis for Repositories',
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help='increase output verbosity')
+    parser.add_argument('-d', '--debug', action='store_true', default=False,
+                        help='enable debug logging')
+    parser.add_argument('-hy', '--hyperparams', action='store_true', default=False,
+                        help='test hyperparams')
+    parser.add_argument('-c', '--cloud', action='store_true', default=False,
+                        help='generate word cloud')
+
+    parser.add_argument('input_file', help='input file')
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        LOGGER.setLevel(logging.INFO)
+    if args.debug:
+        LOGGER.setLevel(logging.DEBUG)
+
+    main(args)
