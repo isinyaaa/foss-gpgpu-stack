83314429d75e6694e2f60ed4722a533af3633161
Isabella Basso <isabbasso@riseup.net>
2022-11-29 15:35:47 -0300

    analyze: add option to use SKLearn for LDA
    Signed-off-by: Isabella Basso <isabbasso@riseup.net>

--- analyze.py
+++ analyze.py
@@ -8,35 +8,93 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 LOGGER = logging.getLogger()
 
 
+class Preprocess:
+    def run(self, data):
+        data.contents = data.contents.apply(self.clean)
+        return self.vectorize(data)
+
+    def clean(self, text):
+        import re
+
+        text = re.sub(r'<.*?>+', '', text)
+        text = re.sub(r'[%s]' % re.escape("""<>'"()[]{}"""), ' ', text)
+        text = re.sub(r'\d*', '', text)
+        text = re.sub(r'\n', '', text)
+        text = re.sub(r'(?:(?:https?:\/\/)|(?:www\.))([^\/ ]+)(?:(?:\.[a-zA-Z]*)|(?::\d{3,5}))(\/+\S*)?', r'\1 \2', text)
+        text = re.sub(r'[%s]' % re.escape("""!"'#$%&*+.,/:;=?@\-^`|~"""), ' ', text)
+        text = re.sub(r'\.+ ', ' ', text)
+        text = re.sub(r' \S ', ' ', text)
+
+        return text
+
+    def vectorize(self, data):
+        import nltk
+        nltk.download('punkt')
+        nltk.download('wordnet')
+        nltk.download('omw-1.4')
+        nltk.download('stopwords')
+        from nltk import word_tokenize
+        from nltk.corpus import stopwords
+        from nltk.stem import WordNetLemmatizer
+
+        from sklearn.feature_extraction.text import CountVectorizer
+
+        stop_words = stopwords.words('english')
+        stop_words.append('www')
+
+        class LemmaTokenizer:
+            def __init__(self):
+                self.wnl = WordNetLemmatizer()
+
+            def __call__(self, doc):
+                return [ self.wnl.lemmatize(t)
+                            for t in word_tokenize(doc)
+                                if t not in stop_words ]
+
+        vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(),
+                                     ngram_range=(1, 3), min_df=3, max_df=0.8,
+                                     lowercase=True, strip_accents='ascii',
+                                     analyzer='word', max_features=1000)
+
+        return vectorizer, vectorizer.fit_transform(data.contents.values)
+
+
 class Cloud:
     """
     Generate a word cloud from the given data
     """
+    def run(self, vectorizer, data):
+        frequencies = self.prepare(vectorizer, data)
+        wordcloud = self.gencloud(frequencies)
+        self.show(wordcloud)
 
-    def prepare(self, data):
-        """
-        Prepare the data for the word cloud
-        """
-        import re
+    def prepare(self, vectorizer, data):
+        import numpy as np
 
-        return data['contents'].map(lambda x: re.sub(r'[,\.!?]', '', x)).map(lambda x: x.lower())
+        frequencies = dict(
+                zip(np.array(vectorizer.get_feature_names_out()),
+                    data.toarray().sum(axis=0))
+                )
 
-    def gencloud(self, words):
+        return frequencies
+
+    def gencloud(self, frequencies):
         """
         Generate the word cloud
         """
         from wordcloud import WordCloud
-        import matplotlib.pyplot as plt
-
-        long_string = ','.join(list(words))
 
         logging.info('Generating word cloud')
 
-        wordcloud = WordCloud(background_color="white", max_words=5000,
-                              contour_width=3, width=1600, height=800,
-                              contour_color='steelblue').generate(long_string)
+        return WordCloud(background_color="white", max_words=5000, width=1600,
+                         height=800, contour_width=3,
+                         contour_color='steelblue').fit_words(frequencies)
 
-        # plot the WordCloud image
+    def show(self, wordcloud):
+        import matplotlib
+        matplotlib.use('TkAgg')
+        import matplotlib.pyplot as plt
+
         plt.figure(figsize = (2, 1), facecolor = None)
         plt.imshow(wordcloud)
         plt.axis("off")
@@ -46,70 +104,81 @@ class Cloud:
 
 
 class LDA:
+    def save_result_as_html(self, prepare, *args, **kwargs):
+        import pyLDAvis
+        import pickle
+
+        # pyLDAvis.enable_notebook()
+        LDAvis_filepath = os.path.join('./ldavis_prepared_' + str(self.topics))
+
+        LDAvis_prepared = prepare(*args, **kwargs)
+        with open(LDAvis_filepath, 'wb') as f:
+            pickle.dump(LDAvis_prepared, f)
+
+        pyLDAvis.save_html(LDAvis_prepared,
+                           os.path.join(LDAvis_filepath) + '.html')
+
+        # pyLDAvis.show(LDAvis_prepared)
+
+
+class SKLearnLDA(LDA):
+    def __init__(self, topics, workers):
+        self.topics = topics
+        self.workers = workers
+
+    def run(self, vectorizer, data):
+        model, results = self.train(data)
+        self.save_result_as_html(model, data, vectorizer)
+        return results
+
+    def train(self, data):
+        from sklearn.decomposition import LatentDirichletAllocation
+
+        if self.workers == None:
+            workers = -1
+        else:
+            workers = self.workers
+
+        lda = LatentDirichletAllocation(n_components=self.topics, max_iter=5,
+                                        learning_method='online',
+                                        learning_offset=50., random_state=0,
+                                        n_jobs=workers)
+        return lda, lda.fit_transform(data)
+
+    def save_result_as_html(self, model, data, vectorizer):
+        from pyLDAvis.sklearn import prepare
+
+        super().save_result_as_html(prepare, model, data, vectorizer, mds='tsne')
+
+
+class GensimLDA(LDA):
     """
     Generate a LDA model from the given data
     """
-    def __init__(self, input_file, alpha, eta, topics, workers):
+    def __init__(self, input_file, data, alpha, eta, topics, workers):
         self.input_file = input_file
+        self.data = data
         self.alpha = alpha
         self.eta = eta
         self.topics = topics
         self.workers = workers
 
-    def prepare(self, data):
-        from gensim import corpora
-        import spacy
+    def run(self, vectorizer, data):
+        _, corpus, id2word = self.prepare(vectorizer, data)
+        model = self.train(corpus, id2word)
+        self.save_result_as_html(model, corpus, id2word)
 
-        data = data.contents.values.tolist()
+    def prepare(self, vectorizer, data):
+        from gensim.matutils import Sparse2Corpus
+        from gensim.corpora import Dictionary
 
-        def remove_stopwords(texts):
-            from gensim.utils import simple_preprocess
-            import nltk
-            nltk.download('stopwords')
-            from nltk.corpus import stopwords
+        corpus = Sparse2Corpus(data, documents_columns=False)
 
-            stop_words = stopwords.words('english')
-            stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'http',
-                               'https', 'com', 'www', 'org', 'net', 'edu',
-                               'gov', 'io'])
-            return [[word for word in simple_preprocess(str(doc), deacc=True)
-                     if word not in stop_words] for doc in texts]
+        texts = vectorizer.inverse_transform(data)
 
-        data = remove_stopwords(data)
+        id2word = Dictionary(texts)
 
-        def make_ngrams(texts):
-            from gensim.models import Phrases, phrases
-
-            bigram = phrases.Phraser(Phrases(data, min_count=5, threshold=100))
-            trigram = phrases.Phraser(Phrases(bigram[data], threshold=100))
-
-            texts = [bigram[doc] for doc in texts]
-
-            return [trigram[bigram[doc]] for doc in texts]
-
-        data = make_ngrams(data)
-
-        def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
-            texts_out = []
-            for sent in texts:
-                doc = nlp(" ".join(sent))
-                texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
-            return texts_out
-
-        nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
-
-        data = lemmatization(data)
-
-        logging.debug("Data entries after LDA prepare")
-        logging.debug(data[:1][0][:30])
-
-        id2word = corpora.Dictionary(data)
-        corpus = [id2word.doc2bow(text) for text in data]
-
-        logging.debug("Corpus entries after LDA prepare")
-        logging.debug(data[:1][0][:30])
-
-        return data, corpus, id2word
+        return texts, corpus, id2word
 
     def train(self, corpus, id2word,
               alpha=None, eta=None, topics=None, workers=None):
@@ -124,19 +193,23 @@ class LDA:
         if workers is None:
             workers = self.workers
 
-        lda_model = LdaMulticore(corpus=corpus, id2word=id2word,
-                                 num_topics=topics, random_state=100,
-                                 chunksize=100, passes=10, alpha=alpha,
-                                 eta=eta, per_word_topics=True,
-                                 workers=workers)
+        model = LdaMulticore(corpus=corpus, id2word=id2word, num_topics=topics,
+                             random_state=100, chunksize=100, passes=10,
+                             alpha=alpha, eta=eta, per_word_topics=True,
+                             workers=workers)
 
         if LOGGER.getEffectiveLevel() == logging.DEBUG:
             from pprint import pprint
-            pprint(lda_model.print_topics())
+            pprint(model.print_topics())
 
-        return lda_model
+        return model
 
-    def test_model_hyperparams(self, data, corpus, id2word,
+    def save_result_as_html(self, model, corpus, id2word):
+        from pyLDAvis.gensim_models import prepare
+
+        super().save_result_as_html(prepare, model, corpus, id2word, None)
+
+    def test_model_hyperparams(self, vectorizer, data,
                                min_alpha=0.1, max_alpha=1, alpha_step=0.3,
                                min_eta=0.1, max_eta=1, eta_step=0.3,
                                min_topics=2, max_topics=11, topics_step=1):
@@ -144,6 +217,8 @@ class LDA:
         import numpy as np
         import tqdm
         from datetime import datetime
+
+        data, corpus, id2word = self.prepare(vectorizer, data)
 
         corpus_sets = [ClippedCorpus(corpus, int(len(corpus) * 0.75)), corpus]
         corpus_title = ['75% Corpus', '100% Corpus']
@@ -165,13 +240,13 @@ class LDA:
             'Coherence': []
         }
 
-        def model_perplexity(data, id2word, lda_model):
+        def model_perplexity(model, texts, id2word):
             """
             Get model perplexity for analysis
             """
             from gensim.models import CoherenceModel
 
-            return CoherenceModel(model=lda_model, texts=data, dictionary=id2word,
+            return CoherenceModel(model=model, texts=texts, dictionary=id2word,
                                   coherence='c_v').get_coherence()
 
         iterations = len(eta) * len(alpha) * len(topics_range) * len(corpus_title)
@@ -183,9 +258,8 @@ class LDA:
                     for b in eta:
                         logging.info("Running LDA with k=%d, a=%s, b=%s" % (k, a, b))
 
-                        lda_model = self.train(corpus_sets[i], id2word,
-                                               a, b, k)
-                        cv = model_perplexity(data, id2word, lda_model)
+                        model = self.train(corpus_sets[i], id2word, a, b, k)
+                        cv = model_perplexity(model, data, id2word)
 
                         model_results['Validation_Set'].append(corpus_title[i])
                         model_results['Topics'].append(k)
@@ -202,49 +276,32 @@ class LDA:
         pbar.close()
         logging.info("LDA tuning results saved to %s" % tuning_results_file)
 
-    def visualize(self, corpus, id2word, lda_model):
-        import pyLDAvis
-        import pyLDAvis.gensim_models as gensimvis
-        import pickle
-
-        # pyLDAvis.enable_notebook()
-        LDAvis_filepath = os.path.join('./ldavis_prepared_' + str(self.topics))
-
-        if True:
-            LDAvis_prepared = gensimvis.prepare(lda_model, corpus, id2word)
-            with open(LDAvis_filepath, 'wb') as f:
-                pickle.dump(LDAvis_prepared, f)
-
-        with open(LDAvis_filepath, 'rb') as f:
-            LDAvis_prepared = pickle.load(f)
-
-        pyLDAvis.save_html(LDAvis_prepared,
-                           os.path.join(LDAvis_filepath) + '.html')
-
-        # pyLDAvis.show(LDAvis_prepared)
 
 def main(args):
     readme_data = pd.read_csv(args.input_file)
     logging.debug(readme_data.head())
-    readme_data = readme_data.drop(columns=['filename'], axis=1)
-    logging.debug(readme_data.head())
+
+    vectorizer, processed_data = Preprocess().run(readme_data)
 
     if args.cloud:
-        cloud = Cloud()
-        words = cloud.prepare(readme_data)
-        cloud.gencloud(words)
-
-    lda = LDA(args.input_file, args.alpha, args.eta, args.topics, args.workers)
-    data, corpus, id2word = lda.prepare(readme_data)
+        Cloud().run(vectorizer, processed_data)
 
     if args.hyperparams:
-        lda.test_model_hyperparams(data, corpus, id2word,
+        lda = GensimLDA(args.input_file, readme_data, args.alpha, args.eta,
+                        args.topics, args.workers)
+
+        lda.test_model_hyperparams(vectorizer, processed_data,
                                    args.min_alpha, args.max_alpha, args.alpha_step,
                                    args.min_eta, args.max_eta, args.eta_step,
                                    args.min_topics, args.max_topics, args.topics_step)
+    if args.gensim:
+        GensimLDA(args.input_file, readme_data,
+                  args.alpha, args.eta, args.topics,
+                  args.workers).run(vectorizer, processed_data)
     else:
-        lda_model = lda.train(corpus, id2word)
-        lda.visualize(corpus, id2word, lda_model)
+        SKLearnLDA(args.topics,
+                   args.workers).run(vectorizer, processed_data)
+
 
 
 if __name__ == '__main__':
@@ -268,14 +325,22 @@ if __name__ == '__main__':
     subparsers = parser.add_subparsers(help='sub-command help')
 
     parser_lda = subparsers.add_parser('lda', help='run LDA')
+    lda_group = parser_lda.add_mutually_exclusive_group(required=True)
+    lda_group.add_argument('-gs', '--gensim', action='store_true',
+                           help='use Gensim implementation')
+    lda_group.add_argument('-sk', '--sklearn', action='store_true',
+                           help='use sklearn implementation')
     parser_lda.add_argument('-t', '--topics', type=int, default=10,
                             help='number of topics to use')
     parser_lda.add_argument('-a', '--alpha', type=float, default=0.8,
                             help='alpha to use')
     parser_lda.add_argument('-e', '--eta', type=float, default=0.8,
                             help='eta to use')
+    parser_lda.add_argument('-tw', '--top-words', type=float, default=20,
+                            help='top words to get on NMF')
+    parser_lda.set_defaults(hyperparams=False) # HACK
 
-    parser_hy = subparsers.add_parser('hy', help='run hyperparameter tuning')
+    parser_hy = subparsers.add_parser('hy', help='run hyperparameter tuning on Gensim LDA')
     parser_hy.add_argument('-mt', '--min-topics', type=int, default=2,
                            help='minimum number of topics to test')
     parser_hy.add_argument('-xt', '--max-topics', type=int, default=11,
