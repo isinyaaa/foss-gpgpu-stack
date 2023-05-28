0c9971d7e6041e22a9b7d3d6d3328f6f2dd8c2dd
Isabella Basso <isabellabdoamaral@usp.br>
2023-05-28 14:09:48 -0300

    RA: scripts: get-gh-data: prefer arrays for bundling data
    Signed-off-by: Isabella Basso <isabellabdoamaral@usp.br>

--- readme-analysis/scripts/get-gh-data.sh
+++ readme-analysis/scripts/get-gh-data.sh
@@ -1,21 +1,60 @@
 #!/usr/bin/env bash
 
-set -o pipefail
-
 DATA_DIR="data/github"
 
-REPO_FLAGS="--stars '>=5' --followers '>=5' --archived=false --visibility 'public' --language 'glsl' --language 'c' --language 'c++' --updated '>=2021-01-01' --limit 1000 --sort 'stars'"
-REPO_OUTPUT="--json fullName,forksCount,stargazersCount,openIssuesCount,updatedAt,description,language"
+REPO_FLAGS=(
+    --stars '>=5'
+    --followers '>=5'
+    --archived=false
+    --visibility 'public'
+    --language 'glsl'
+    --language 'c'
+    --language 'c++'
+    --updated '>=2021-01-01'
+    --limit 1000
+    --sort 'stars'
+)
+REPO_OUTPUT=(
+    fullName
+    forksCount
+    stargazersCount
+    openIssuesCount
+    updatedAt
+    description
+    language
+)
+repo_output=$(IFS=,; echo "${REPO_OUTPUT[*]}")
 
-ISSUE_FLAGS="--sort 'updated' --order 'desc' --limit 1000"
-ISSUE_OUTPUT="--json createdAt,updatedAt,commentsCount,state"
+ISSUE_FLAGS=(
+    --sort 'updated'
+    --order 'desc'
+    --limit 1000
+)
+ISSUE_OUTPUT=(
+    createdAt
+    updatedAt
+    commentsCount
+    state
+)
+issue_output=$(IFS=,; echo "${ISSUE_OUTPUT[*]}")
 
 cd "$DATA_DIR" || exit 1
 
 for api in {cuda,opencl,opengl,vulkan}; do
     echo "Getting $api data"
-    eval gh search repos "$api" "$REPO_FLAGS" "$REPO_OUTPUT" | jq | tee "${api}_output".json
-    cat "${api}_output".json | jq 'map(.fullName)' | sed -e 's/"//g' -e 's/,//g' -e '1d;$d' | tee "${api}_repos".txt
+    repo_query_out="${api}.json"
+
+    (
+        set -o pipefail
+        gh search repos $api "${REPO_FLAGS[@]}" --json "$repo_output" | jq > "$repo_query_out"
+    )
+
+    repo_list="${api}_repos.txt"
+    # we map the fullName from each entry to a list
+    # then we remove the first and last lines (the brackets), quotes and commas
+    jq 'map(.fullName)' "$repo_query_out" |\
+        sed -e 's/"//g' -e 's/,//g' -e '1d;$d' > "$repo_list"
+
     while read -r repo; do
         echo "Getting $repo issues"
         issue_query_out=$(mktemp)
@@ -23,9 +62,8 @@ for api in {cuda,opencl,opengl,vulkan}; do
             slept=0
             tmp=$(mktemp)
             while [ $slept -lt 600 ]; do
-                eval gh search issues --repo "$remote" "$ISSUE_FLAGS" --state "$state" "$ISSUE_OUTPUT" > "$tmp"
-                if [ $? -ne 0 ]; then
-                    echo "Failed to get issues for $remote"
+                if gh search issues --repo "$repo" "${ISSUE_FLAGS[@]}" --state $state --json "$issue_output" > "$tmp"; then
+                    echo "Failed to get issues for $repo"
                     sleep 5
                     slept=$((slept + 5))
                 else
@@ -41,5 +79,5 @@ for api in {cuda,opencl,opengl,vulkan}; do
         # fallback for an empty list or a trailing comma
         echo '[]' >> "$issue_query_out"
         jq -s 'flatten' "$issue_query_out" > "${api}/${repo//\//_}.json"
-    done < "${api}_repos".txt
+    done < "$repo_list"
 done
